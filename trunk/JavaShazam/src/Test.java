e436f708853560f327f49b8437ede43d3494654f
mariottf2@gmail.com <mariottf2@gmail.com@5ffe00a4-90cd-98b8-1ca3-e836b52c10ff>
2013-05-15 20:38:31 +0000

    Last version

--- trunk/JavaShazam/src/Test.java
+++ trunk/JavaShazam/src/Test.java
@@ -14,22 +14,22 @@ import FingerPrinting.description.MPEG7Description;
 public class Test {
 	
 	public static void main(String...args) throws UnsuportedSampleRateException, FileNotFoundException{
-//	String inputFile = "Sehnsucht with Xavier Naidoo.mp3";
+	//String inputFile = "Sehnsucht with Xavier Naidoo.mp3";
 //	String outputXMLFile = "Sehnsucht with Xavier Naidoo.xml";
 		
 //String inputFile = "sub_(40-60)_Sehnsucht with Xavier Naidoo.mp3";
 	//String outputXMLFile = "sub_(40-60)_Sehnsucht with Xavier Naidoo.xml";
 	
-//   String inputFile = "Wait In Vain (Avicii Remix).mp3";
-  // String outputXMLFile = "Wait In Vain (Avicii Remix).xml";
+ //  String inputFile = "Wait In Vain (Avicii Remix).mp3";
+ //String outputXMLFile = "Wait In Vain (Avicii Remix).xml";
 	
 //String inputFile = "Wait In Vain (Avicii Remix)_crop.mp3";
 //String outputXMLFile = "Wait In Vain (Avicii Remix)_crop.xml";
 	
 	String inputFile = "rec.mp3";
-	String outputXMLFile = "rec.xml";
+String outputXMLFile = "rec.xml";
 		
-	//	String inputFile = "1-05 Clocks.mp3";
+		//String inputFile = "1-05 Clocks.mp3";
 	//	String outputXMLFile = "1-05 Clocks.xml";
 		
 		long currentTime = System.currentTimeMillis();
@@ -43,7 +43,7 @@ public class Test {
 		try {
 			description.createNewFile(outputXMLFile);
 			description.setAudioInfo("2", inputFile);
-			description.setTimeStampStep(400);
+			description.setTimeStampStep(20);
 			description.setFingerPrint(hashes);
 			description.write();
 			
